be64c246b9c70ce6b318d36d604b0440c66c5ac5
KenLZY <112811713+KenLZY@users.noreply.github.com>
2025-05-20 16:48:22 +0800

    update(feat): added console logs
    - This is to demonstrate which callbacks come first

--- l1_1_ex/textandbutton.js
+++ l1_1_ex/textandbutton.js
@@ -1,10 +1,12 @@
 function handleButton1Click() {
+    console.log("handleBUtton1Click() is being run!");
     var textbox1 = document.getElementById("textbox1");
     var span1 = document.getElementById("span1");
     span1.innerHTML = textbox1.value;
 }
 
 function run() {
+    console.log("run() is being run!");
     var button1 = document.getElementById("button1");
     button1.addEventListener("click", handleButton1Click); 
 }
