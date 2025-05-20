e02ebf6adf849b7cceb5b08ecf1122b52cf54b5d
KenLZY <112811713+KenLZY@users.noreply.github.com>
2025-05-20 16:36:12 +0800

    update(feat): update hello.js

--- l1_1_ex/hello.js
+++ l1_1_ex/hello.js
@@ -3,10 +3,11 @@ var msg = "hello";
  alert(msg); */
 // document.write(msg);
 
-
+// function definition in JavaScript
 function run() {
     var p_elem = document.getElementById("msg1");
     p_elem.innerText = msg;
 
 }
+// the function run is called when the DOM is loaded
 document.addEventListener( "DOMContentLoaded", run);
